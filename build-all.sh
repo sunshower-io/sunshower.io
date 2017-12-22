40cbf4880983364ccd307f026e4c4d7b502e504e
josiahhaswell <josiah.haswell@gmail.com>
2017-12-22 11:12:39 -0700

    Adding sunshower-core

--- build-all.sh
+++ build-all.sh
@@ -1,1 +1,10 @@
+#!/bin/bash -eu
+
+docker build -t sunshower-site -f Dockerfile .   
+docker run -it --rm -e  MVN_REPO_USERNAME=myMavenRepo \
+-e MVN_REPO_PASSWORD=lid-DOG-bin-123  \
+-e MVN_REPO_URL=https://mymavenrepo.com/repo/IRgrTxMdF4OnnbNAkfnJ \
+-e SITEMASTER_GITHUB_USERNAME=sunshower-robot \
+-e SITEMASTER_GITHUB_PASSWORD=mzlapQ123 \
+--name "sunshower-site" "sunshower-site" 
 
