f605f4343d5c30052f87260773fff1c1d7858fbe
Josiah Haswell <josiah.haswell@gmail.com>
2018-07-20 00:36:58 -0600

    setting up form

--- new/js/form_script.js
+++ new/js/form_script.js
@@ -5,124 +5,124 @@
  * Compatible with jqery validator plugin
  */
 
-(function ($) {
-
-    $.fn.initForm = function (options) {
-        var settings = $.extend({
-            type: 'post',
-            // serverUrl: '#',
-            // serverUrl: '/new/ajaxserver/server.php',
-            serverUrl: 'http://www.google.com',
-            successClean: this.find('.form-success-clean'),
-            successGone: this.find('.form-success-gone'),
-            successInvisible: this.find('.form-success-invisible'),
-            successVisible: this.find('.form-success-visible'),
-            textFeedback: this.find('.form-text-feedback'),
-        }, options);
-        var $ajax = {
-            sendRequest: function (p) {
-                var form_fill = $(p);
-
-                // Get the form data.
-                var form_inputs = form_fill.find(':input');
-                var form_data = {};
-                form_inputs.each(function () {
-                    var value = $(this).val(),
-                        name = this.name;
-
-                    if (name || value) {
-                        if (!name) {
-                            form_data[value] = value;
-                        } else {
-                            form_data[name] = value;
-                        }
-                    }
-                });
-                $.ajax(
-                    {
-                        /*
-                         *Your Ajax Server Here,
-                         * use internal url (such as './ajaxserver/server.php') or
-                         * external URL such as:  url: 'http://www.example.com/avenir/ajaxserver/server.php'
-                         * depending to your requirements
-                         */
-                        // url: settings.serverUrl,
-                        url: './ajaxserver/server.php',
-                        type: settings.type,
-                        data: JSON.stringify(form_data),
-                        dataType: 'json',
-
-                        /* CALLBACK FOR SENDING EMAIL GOEAS HERE */
-                        success: function (data) {
-                            //Ajax connexion was a success, now handle response
-                            if (data && !data.error) {
-                                // Hide for if no error
-                                settings.successClean.val("");
-                                settings.successInvisible.addClass('invisible');
-                                settings.successGone.addClass('gone');
-                                settings.successVisible.removeClass('invisible');
-                                settings.successVisible.removeClass('gone');
-                                console.log('Request sent successfully');
-                            }
-                            // Else the login credentials were invalid.
-                            else {
-                                //Ajax connexion reject an error a success, now handle response
-                                settings.textFeedback.removeClass('gone');
-                                settings.textFeedback.removeClass('invisible');
-                                settings.textFeedback.html('Error when sending request.');
-                                console.log('Could not process AJAX request to server');
-                            }
-                        },
-                        /* show error message */
-                        error: function (jqXHR, textStatus, errorThrown) {
-                            //ajax error
-                            settings.textFeedback.removeClass('gone');
-                            settings.textFeedback.removeClass('invisible');
-                            settings.textFeedback.html('Error when sending request.');
-                            console.log('ajax error');
-
-                        }
-                        /* END EMAIL SENDING CALLBACK */
-                    });
-            }
-
-        };
-
-
-        //if jquery validator plugin is enable, use it
-        if (jQuery.validator) {
-            jQuery.validator.setDefaults({
-                success: "valid"
-            });
-            this.validate({
-                rules: {
-                    field: {
-                        required: true,
-                        email: true
-                    }
-                }
-            });
-        }
-
-
-        this.submit(function (event) {
-            // prevent default submit
-            console.log('Send request');
-            event.preventDefault();
-            // use jquery validator plugin if it is enabled
-            if (jQuery.validator) {
-                if ($(this).valid()) {
-                    $ajax.sendRequest(this);
-                }
-            }
-            else {
-                $ajax.sendRequest(this);
-            }
-        });
-
-    };
-
-}(jQuery));
+// (function ($) {
+//
+//     $.fn.initForm = function (options) {
+//         var settings = $.extend({
+//             type: 'post',
+//             // serverUrl: '#',
+//             // serverUrl: '/new/ajaxserver/server.php',
+//             serverUrl: 'http://www.google.com',
+//             successClean: this.find('.form-success-clean'),
+//             successGone: this.find('.form-success-gone'),
+//             successInvisible: this.find('.form-success-invisible'),
+//             successVisible: this.find('.form-success-visible'),
+//             textFeedback: this.find('.form-text-feedback'),
+//         }, options);
+//         var $ajax = {
+//             sendRequest: function (p) {
+//                 var form_fill = $(p);
+//
+//                 // Get the form data.
+//                 var form_inputs = form_fill.find(':input');
+//                 var form_data = {};
+//                 form_inputs.each(function () {
+//                     var value = $(this).val(),
+//                         name = this.name;
+//
+//                     if (name || value) {
+//                         if (!name) {
+//                             form_data[value] = value;
+//                         } else {
+//                             form_data[name] = value;
+//                         }
+//                     }
+//                 });
+//                 $.ajax(
+//                     {
+//                         /*
+//                          *Your Ajax Server Here,
+//                          * use internal url (such as './ajaxserver/server.php') or
+//                          * external URL such as:  url: 'http://www.example.com/avenir/ajaxserver/server.php'
+//                          * depending to your requirements
+//                          */
+//                         // url: settings.serverUrl,
+//                         url: './ajaxserver/server.php',
+//                         type: settings.type,
+//                         data: JSON.stringify(form_data),
+//                         dataType: 'json',
+//
+//                         /* CALLBACK FOR SENDING EMAIL GOEAS HERE */
+//                         success: function (data) {
+//                             //Ajax connexion was a success, now handle response
+//                             if (data && !data.error) {
+//                                 // Hide for if no error
+//                                 settings.successClean.val("");
+//                                 settings.successInvisible.addClass('invisible');
+//                                 settings.successGone.addClass('gone');
+//                                 settings.successVisible.removeClass('invisible');
+//                                 settings.successVisible.removeClass('gone');
+//                                 console.log('Request sent successfully');
+//                             }
+//                             // Else the login credentials were invalid.
+//                             else {
+//                                 //Ajax connexion reject an error a success, now handle response
+//                                 settings.textFeedback.removeClass('gone');
+//                                 settings.textFeedback.removeClass('invisible');
+//                                 settings.textFeedback.html('Error when sending request.');
+//                                 console.log('Could not process AJAX request to server');
+//                             }
+//                         },
+//                         /* show error message */
+//                         error: function (jqXHR, textStatus, errorThrown) {
+//                             //ajax error
+//                             settings.textFeedback.removeClass('gone');
+//                             settings.textFeedback.removeClass('invisible');
+//                             settings.textFeedback.html('Error when sending request.');
+//                             console.log('ajax error');
+//
+//                         }
+//                         /* END EMAIL SENDING CALLBACK */
+//                     });
+//             }
+//
+//         };
+//
+//
+//         //if jquery validator plugin is enable, use it
+//         if (jQuery.validator) {
+//             jQuery.validator.setDefaults({
+//                 success: "valid"
+//             });
+//             this.validate({
+//                 rules: {
+//                     field: {
+//                         required: true,
+//                         email: true
+//                     }
+//                 }
+//             });
+//         }
+//
+//
+//         this.submit(function (event) {
+//             // prevent default submit
+//             console.log('Send request');
+//             event.preventDefault();
+//             // use jquery validator plugin if it is enabled
+//             if (jQuery.validator) {
+//                 if ($(this).valid()) {
+//                     $ajax.sendRequest(this);
+//                 }
+//             }
+//             else {
+//                 $ajax.sendRequest(this);
+//             }
+//         });
+//
+//     };
+//
+// }(jQuery));
 
 /* End of ajax */
 
