8b870fa11576e6ee6532e60d4e3aae11ebab0fa7
Josiah Haswell <josiah.haswell@gmail.com>
2018-07-23 16:22:47 -0600

    Adding schnorps

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-gradle clean assemble \
--PmavenRepositoryUrl=${MVN_REPO_URL} \
--PmavenRepositoryUsername=${MVN_REPO_USERNAME} \
--PmavenRepositoryPassword=${MVN_REPO_PASSWORD} \
--Pversion=1.0.0.Final \
--Penv.version=1.0.0.Final
+#gradle clean assemble \
+#-PmavenRepositoryUrl=${MVN_REPO_URL} \
+#-PmavenRepositoryUsername=${MVN_REPO_USERNAME} \
+#-PmavenRepositoryPassword=${MVN_REPO_PASSWORD} \
+#-Pversion=1.0.0.Final \
+#-Penv.version=1.0.0.Final
 
 
 git config --global user.email "$SITEMASTER_GITHUB_USERNAME"
@@ -14,11 +14,13 @@ git config --global credential.helper 'cache --timeout 7200'
 git config --global credential.helper cache
 
 git clone https://github.com/sunshower-io/sunshower.io.git
-cp -r target/* sunshower.io/docs 
-cd sunshower.io
-
-git config remote.origin.url https://${SITEMASTER_GITHUB_USERNAME}:${SITEMASTER_GITHUB_PASSWORD}@github.com/sunshower-io/sunshower.io.git
-git add .
-git commit -am "Updating site from CD"
-git push origin master
+pushd ./new && gulp clean build && popd
+mkdir -p sunshower.io/docs
+cp -r new/* sunshower.io/docs
+cp -r CNAME sunshower.io/docs
+cd sunshower.io &&
+git config remote.origin.url https://${SITEMASTER_GITHUB_USERNAME}:${SITEMASTER_GITHUB_PASSWORD}@github.com/sunshower-io/sunshower.io.git &&
+git add . &&
+git commit -am "Updating site from CD" &&
+git push -f origin master
 
